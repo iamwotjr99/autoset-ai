7971808ec8ed4ab6e8dbdc9b2fd2e874306dc06e
iamwotjr99 <iamwotjr@gmail.com>
2026-08-19 15:18:18 +0900

    master: server init

---
+++ backend/src/main/java/com/autosetai/backend/AutosetAiApplication.java
@@ -0,0 +1,13 @@
+package com.autosetai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosetAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosetAiApplication.class, args);
+	}
+
+}

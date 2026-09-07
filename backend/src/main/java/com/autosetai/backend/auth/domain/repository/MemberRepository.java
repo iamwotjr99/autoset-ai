b886059d773629ecd7833c7993af5e9676a1dbb4
iamwotjr99 <iamwotjr@gmail.com>
2026-09-07 11:15:11 +0900

    [feat] MemberRepository 구현

--- backend/src/main/java/com/autosetai/backend/auth/domain/repository/MemberRepository.java
+++ backend/src/main/java/com/autosetai/backend/auth/domain/repository/MemberRepository.java
@@ -1,4 +1,9 @@
 package com.autosetai.backend.auth.domain.repository;
 
-public interface MemberRepository {
+import com.autosetai.backend.auth.domain.model.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findBySocialId(String socialId);
 }

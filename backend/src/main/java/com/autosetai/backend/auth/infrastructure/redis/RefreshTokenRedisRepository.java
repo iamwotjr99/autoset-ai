b70c0986ef9ff40e767b6d2fac0496b5ec4d54ab
iamwotjr99 <iamwotjr@gmail.com>
2026-09-07 13:24:17 +0900

    [feat] RefreshToken Redis Repository 구현

---
+++ backend/src/main/java/com/autosetai/backend/auth/infrastructure/redis/RefreshTokenRedisRepository.java
@@ -0,0 +1,41 @@
+package com.autosetai.backend.auth.infrastructure.redis;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RefreshTokenRedisRepository {
+    private static final String KEY_PREFIX = "RT:";
+
+    private final StringRedisTemplate stringTemplate;
+    private final long refreshTokenExpiration;
+
+    public RefreshTokenRedisRepository(
+            StringRedisTemplate stringRedisTemplate,
+            @Value("${jwt.refresh-expiration") long refreshTokenExpiration
+    ) {
+        this.stringTemplate = stringRedisTemplate;
+        this.refreshTokenExpiration = refreshTokenExpiration;
+    }
+
+    // refreshToken 저장
+    public void save(Long memberId, String refreshToken) {
+        String key = KEY_PREFIX + memberId;
+        stringTemplate.opsForValue()
+                .set(key, refreshToken, Expiration.from(refreshTokenExpiration, TimeUnit.MILLISECONDS));
+    }
+
+    // memberId를 통해 refreshToken 조회
+    public Optional<String> getRefreshTokenByMemberId(Long memberId) {
+        return Optional.ofNullable(stringTemplate.opsForValue().get(KEY_PREFIX + memberId));
+    }
+
+    // memberId를 통해 refreshToken 삭제
+    public void deleteRefreshTokenByMemberId(Long memberId) {
+        stringTemplate.delete(KEY_PREFIX + memberId);
+    }
+}
--- backend/src/main/java/com/autosetai/backend/auth/service/AuthService.java
+++ backend/src/main/java/com/autosetai/backend/auth/service/AuthService.java
@@ -1,4 +1,48 @@
 package com.autosetai.backend.auth.service;
 
+import com.autosetai.backend.auth.domain.model.Member;
+import com.autosetai.backend.auth.domain.repository.MemberRepository;
+import com.autosetai.backend.auth.infrastructure.oauth.OAuthClient;
+import com.autosetai.backend.auth.infrastructure.oauth.OAuthUserProfile;
+import com.autosetai.backend.auth.util.RandomNicknameGenerator;
+import com.autosetai.backend.auth.util.jwt.JwtProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final OAuthClient oAuthClient;
+    private final RandomNicknameGenerator nicknameGenerator;
+    private final JwtProvider jwtProvider;
+
+    public String socialLogin(String socialAccessToken) {
+
+        // 1. socialAccessToken으로 social 인증
+        OAuthUserProfile userProfile = oAuthClient.getUserProfile(socialAccessToken);
+
+        // 2. social_id를 통해 우리 서비스의 유저인지 확인
+        // 우리 서비스의 유저가 아니라면 회원가입(Member 생성 및 저장)
+        Member member = memberRepository.findBySocialId(userProfile.socialId())
+                .orElseGet(() -> {
+                    String nickname = nicknameGenerator.generate();
+
+                    Member newMember = Member.create(userProfile.socialId(), userProfile.provider(),
+                            nickname);
+
+                    return memberRepository.save(newMember);
+                });
+
+        /**
+         * 토큰 생성 로직
+         */
+        // 3. 로그인 성공: accessToken 발급, refreshToken 저장
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+        String refreshToken = jwtProvider.createRefreshToken(member.getId());
+
+        return accessToken;
+    }
+
 }
